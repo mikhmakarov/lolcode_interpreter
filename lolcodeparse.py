275ebcfee5f13bd58be6bfad4b16904418026816
Mikhail Makarov <makarov.mikhail11@gmail.com>
2018-10-04 16:54:37 +0300

    implemented math expr

--- lolcodeparse.py
+++ lolcodeparse.py
@@ -4,30 +4,135 @@ import lolcodelex
 
 tokens = lolcodelex.tokens
 
+VISIBLE = '<VISIBLE>'
+GIMMEH = '<GIMMEH>'
+SUM = '<SUM>'
+DIFF = '<DIFF>'
+PRODUKT = '<PRODUKT>'
+QUOSHUNT = '<QUOSHUNT>'
+MOD = '<MOD>'
+BIGGR = '<BIGGR>'
+SMALLR = '<SMALLR>'
+
+
 
 def p_program(p):
-    '''program : HAI FLOAT statements KTHXBYE'''
-    if not p[0]:
-        p[0] = []
+    '''program : HAI FLOAT NEWLINE statements KTHXBYE'''
+    p[0] = p[4] if p[4] else []
 
 
 def p_statements(p):
     '''statements : statements statement
                   | statement'''
-    pass
+    if len(p) == 2:
+        p[0] = []
+        if p[1]:
+            p[0].append(p[1])
+    else:
+        p[0] = p[1] if p[1] else []
+
+        if p[2]:
+            p[0].append(p[2])
 
 
 def p_statement(p):
-    '''statement : VISIBLE STRING'''
-    pass
+    '''statement : command NEWLINE
+                 | command COMMA'''
+    p[0] = p[1]
+
+
+def p_command_empty(p):
+    '''command : empty'''
+
+
+def p_command_expr(p):
+    '''command : expr'''
+    p[0] = p[1]
+
+
+def p_command_call(p):
+    '''command : call'''
+    p[0] = p[1]
+
+
+def p_args(p):
+    '''args : args expr
+            | expr'''
+    if len(p) == 2:
+        p[0] = [p[1]]
+    else:
+        p[0] = p[1] if p[1] else []
+
+        if p[2]:
+            p[0].append(p[2])
+
+
+def p_call_visible_newline(p):
+    '''call : VISIBLE args
+            | VISIBLE args EXCLAMATION'''
+    newline = len(p) == 3
+    p[0] = (VISIBLE, p[2], newline)
+
+
+def p_call_gimmeh(p):
+    '''expr : GIMMEH variable'''
+    p[0] = (GIMMEH, p[2])
+
+
+def p_expr_string(p):
+    '''expr : STRING'''
+    p[0] = p[1][1:-1]
+
+
+def p_expr_float(p):
+    '''expr : FLOAT'''
+    p[0] = float(p[1])
+
+
+def p_expr_int(p):
+    '''expr : INTEGER'''
+    p[0] = int(p[1])
+
+
+def p_expr_math(p):
+    '''expr : SUM OF expr AN expr
+            | DIFF OF expr AN expr
+            | PRODUKT OF expr AN expr
+            | QUOSHUNT OF expr AN expr
+            | MOD OF expr AN expr
+            | BIGGR OF expr AN expr
+            | SMALLR OF expr AN expr'''
+    op = None
+    if p[1] == 'SUM':
+        op = SUM
+    elif p[1] == 'DIFF':
+        op = DIFF
+    elif p[1] == 'PRODUKT':
+        op = PRODUKT
+    elif p[1] == 'QUOSHUNT':
+        op = QUOSHUNT
+    elif p[1] == 'MOD':
+        op = MOD
+    elif p[1] == 'BIGGR':
+        op = BIGGR
+    elif p[1] == 'SMALLR':
+        op = SMALLR
+    else:
+        print('unknown math operator', p[1])
+        p.parser.error = 1
+
+    p[0] = (op, [p[3], p[5]])
+
+
+def p_variable(p):
+    '''variable : ID'''
+    p[0] = p[1]
 
 
 def p_program_error(p):
     '''program : error'''
     p[0] = None
     p.parser.error = 1
-
-
 
 
 # Empty
