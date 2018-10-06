848d7884dab3149fc11b343265de3d248f9c013c
Mikhail Makarov <makarov.mikhail11@gmail.com>
2018-10-06 19:11:21 +0300

    implemented binary expressions

--- lolcodeinterp.py
+++ lolcodeinterp.py
@@ -20,8 +20,9 @@ class LolCodeInterpreter(object):
 
     def process_statements(self, statements):
         for statement in statements:
-            if statement[0] == EXPR:
-                self.process_expr(statement[1])
+            node_type, value = statement
+            if node_type == EXPR:
+                self.process_expr(value)
 
     def expr_res(self, res):
         self.it = res
@@ -33,19 +34,21 @@ class LolCodeInterpreter(object):
             return self.expr_res(self.process_value(value))
         if node_type == VAR:
             return self.expr_res(self.process_variable(value))
-
+        if node_type in [SUM, DIFF, PRODUKT, QUOSHUNT, MOD, BIGGR, SMALLR]:
+            return self.expr_res(self.process_binary(node_type, value))
 
     def process_value(self, val):
-        if val[0] == YARN:
-            return val[1]
-        elif val[0] == NUMBR:
-            return int(val[1])
-        elif val[0] == NUMBAR:
-            return float(val[1])
-        elif val[0] == TROOF:
-            if val[1] == WIN:
+        node_type, value = val
+        if node_type == YARN:
+            return value
+        if node_type == NUMBR:
+            return int(value)
+        if node_type == NUMBAR:
+            return float(value)
+        if node_type == TROOF:
+            if value == WIN:
                 return True
-            elif val[1] == FAIL:
+            elif value == FAIL:
                 return False
             else:
                 raise Exception('unknown value for TROOF type')
@@ -58,4 +61,23 @@ class LolCodeInterpreter(object):
             return self.vars[var_name]
 
         raise Exception('variable {}: used before declaration'.format(var_name))
+
+    def process_binary(self, op, args):
+        lhs = args[0][1]
+        rhs = args[1][1]
+
+        if op == SUM:
+            return self.process_expr(lhs) + self.process_expr(rhs)
+        if op == DIFF:
+            return self.process_expr(lhs) - self.process_expr(rhs)
+        if op == PRODUKT:
+            return self.process_expr(lhs) * self.process_expr(rhs)
+        if op == QUOSHUNT:
+            return self.process_expr(lhs) / self.process_expr(rhs)
+        if op == MOD:
+            return self.process_expr(lhs) % self.process_expr(rhs)
+        if op == BIGGR:
+            return max(self.process_expr(lhs), self.process_expr(rhs))
+        if op == SMALLR:
+            return min(self.process_expr(lhs), self.process_expr(rhs))
 
