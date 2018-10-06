0979da85304927f8ed910631dee98a3c39da68b3
Mikhail Makarov <makarov.mikhail11@gmail.com>
2018-10-06 08:51:24 +0300

    moved constants to a separate file

--- lolcodeparse.py
+++ lolcodeparse.py
@@ -1,36 +1,10 @@
 from ply import *
 
 import lolcodelex
+from constants import *
+
 
 tokens = lolcodelex.tokens
-
-VISIBLE = '<VISIBLE>'
-GIMMEH = '<GIMMEH>'
-SUM = '<SUM>'
-DIFF = '<DIFF>'
-PRODUKT = '<PRODUKT>'
-QUOSHUNT = '<QUOSHUNT>'
-MOD = '<MOD>'
-BIGGR = '<BIGGR>'
-SMALLR = '<SMALLR>'
-BOTH = '<BOTH>'
-EITHER = '<EITHER>'
-WON = '<WON>'
-NOT = '<NOT>'
-ALL = '<ALL>'
-ANY = '<ANY>'
-SAME = '<SAME>'
-DIFFRINT = '<DIFFRINT>'
-SMOOSH = '<SMOOSH>'
-MAEK = '<MAEK>'
-CAST = '<CAST>'
-DECLARE = '<DECLARE>'
-ASSIGN = '<ASSIGN>'
-IF_ELSE = '<IF_ELSE>'
-YARN = '<YARN>'
-TROOF = '<TROOF>'
-NUMBR = '<NUMBR>'
-NUMBAR = '<NUMBAR>'
 
 
 def p_program(p):
